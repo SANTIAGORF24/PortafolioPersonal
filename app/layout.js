b89a47e0a0396f53629c62c4d37e2697eee0a5bf
SANTIAORF24 <santiforero57@gmail.com>
2025-01-31 10:43:33 -0500

    analytics vercerl

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,16 @@
 "use client";
 import "./globals.css";
 import { HeroUIProvider } from "@heroui/react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body className="scroll-smooth">
-        <HeroUIProvider>{children}</HeroUIProvider>
+        <HeroUIProvider>
+          {children}
+          <Analytics />
+        </HeroUIProvider>
       </body>
     </html>
   );
